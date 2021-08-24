402ae979a9f6abf4ea73ed28a0bd3d8d5d3570ae
wgPassos <passoswilliangabriel@gmail.com>
2021-08-23 22:53:28 -0300

    Melhorando Js

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 let n1 = document.querySelector("#n1");
 let n2 = document.querySelector("#n2");
+let button = document.querySelector("button");
 let resultado = document.querySelector("span");
 
-function somar() {
+// function inputVazio;
+// button.addEventListener("click", inputVazio);
+
+function inputVazio() {
+    if ((n1.value === "" || n2.value === "")) {
+        // resultado.style.display = "none";
+        alert("Insert a number in the inputs.");
+        resultado.value = "";
+    }
+}
+
+function somar() {   
+    inputVazio();
     resultado.innerHTML = parseFloat(n1.value) + parseFloat(n2.value);
+    n1.value = "";
+    n2.value = "";
 }
 
 function subtrair() {
+    inputVazio();
     resultado.innerHTML = parseFloat(n1.value) - parseFloat(n2.value);
+    n1.value = "";
+    n2.value = "";
 }
 
 function multiplicar() {
+    inputVazio();
     resultado.innerHTML = parseFloat(n1.value) * parseFloat(n2.value);
+    n1.value = "";
+    n2.value = "";
 }
 
 function dividir() {
+    inputVazio();
     resultado.innerHTML = parseFloat(n1.value) / parseFloat(n2.value);
+    n1.value = "";
+    n2.value = "";
 }
