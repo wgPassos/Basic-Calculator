aa526f7a2b6d9b5fc1fda436e877878153ae3db0
wgPassos <passoswilliangabriel@gmail.com>
2021-09-07 14:53:19 -0300

    Aplicado :focus nos inputs

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function inputVazio() {
     if (n1.value === "" || n2.value === "")  {          
         alert("Enter a number in all fields."); 
     limparResultado();
-        if (resultado !== "") {
-            limparResultado();
-        }
+        // if (resultado !== "") {
+        //     limparResultado();
+        // }
     }
 }
 
