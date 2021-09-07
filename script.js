191bbae204fd38211e2238c8031bf90bea509375
wgPassos <passoswilliangabriel@gmail.com>
2021-09-07 10:13:03 -0300

    modificando textos

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ let n2 = document.querySelector("#n2");
 let button = document.querySelector("button");
 let resultado = document.querySelector("span");
 
-// function inputVazio;
-// button.addEventListener("click", inputVazio);
 
 function inputVazio() {
-    if ((n1.value === "" || n2.value === "")) {
-        // resultado.style.display = "none";
-        alert("Insert a number in the inputs.");
-        resultado.value = "";
+    if (n1.value === "" || n2.value === "")  {          
+        alert("Enter a number in all fields."); 
+    limparResultado();
+        if (resultado !== "") {
+            limparResultado();
+        }
     }
 }
 
-function somar() {   
+function limparResultado () {
+    let resultado, n1, n2;
+    resultado.innerHTML = "";
+    n1.innerHTML = "";
+    n2.innerHTML = "";
+}
+
+function somar() {
     inputVazio();
     resultado.innerHTML = parseFloat(n1.value) + parseFloat(n2.value);
     n1.value = "";
